f935d3d6382761186188339826f59bf9c2376772
Omar Eddari <eddari.omar.97@gmail.com>
2026-05-20 18:45:19 +0100

    init

---
+++ src/main/java/com/example/activite_pratique_n2_orm_jpa_hibernate_spring_data/ActivitePratiqueN2OrmJpaHibernateSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.activite_pratique_n2_orm_jpa_hibernate_spring_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitePratiqueN2OrmJpaHibernateSpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitePratiqueN2OrmJpaHibernateSpringDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/activite_pratique_n2_orm_jpa_hibernate_spring_data/ActivitePratiqueN2OrmJpaHibernateSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.activite_pratique_n2_orm_jpa_hibernate_spring_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitePratiqueN2OrmJpaHibernateSpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
